fa28dabbdebac36e0c3e6dda351e2a32999c3312
Jason Nathannael <Jasonnathannael05@gmail.com>
2024-11-23 16:00:25 +0700

    Menambahkan tombol settings dan asset photo Profile

--- app.js
+++ app.js
@@ -8,10 +8,30 @@ var app = angular.module('musicPlayerApp', ['ngRoute'])
                 .when('/currentSong', {
                     templateUrl: 'templates/currentSong.html',
                 })
+                .when('/login', {
+                    templateUrl: 'templates/login.html',
+                })
                 .when('/register', {
                     templateUrl: 'templates/register.html',
+                })
+                .when('/settings', {
+                    templateUrl: 'templates/settings.html',
+                    controller: 'SettingsController'
                 })
                 .otherwise({
                 redirectTo: '/'
                 });
+        }]);
+
+        app.controller('SettingsController', ['$scope', function ($scope) {
+            // Placeholder user data
+            $scope.userName = "John Doe";
+            $scope.userEmail = "johndoe@example.com";
+        
+            // Logout function
+            $scope.logout = function () {
+                alert("You have successfully logged out!");
+                // Tambahkan logika logout sesuai kebutuhan, misalnya navigasi ke halaman login
+                window.location.href = "#/login";
+            };
         }]);
