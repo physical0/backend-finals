a8b23ed3082eecedaf4eb94f6a8b57374632ab15
axel <belaxist@gmail.com>
2024-12-06 12:59:58 +0700

    library functionality
    library view, adding and removing albums from library database, library service, details

--- api.js
+++ api.js
@@ -27,14 +27,49 @@ app.use(express.json());
 
 app.post("/api/upload/library", async (req, res) => {
   console.log(req.body);
+  const allBody = req.body;
 
   try {
-    const newAlbum = new Album(req.body);
+    const newAlbum = new Album(allBody);
 
     if (newAlbum) {
       const savedAlbum = await newAlbum.save();
       res.status(201).json({ message: "Upload successful", album: newAlbum });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/api/library", async (req, res) => {
+
+  try {
+  const albums = await Album.find({inLibrary: true});
+
+  const results = [];
+
+  albums.forEach(album => {
+    results.push({
+      id: album.id,
+      title: album.title,
+      year: album.year,
+      dateAdded: album.dateAdded,
+      thumb: album.thumb,
+    });
+  });
+   res.status(200).json(results);
+   
+   return results;
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/api/library/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedAlbum = await Album.deleteOne({id: id});
+    res.status(200).json({ message: "Album removed from library" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
