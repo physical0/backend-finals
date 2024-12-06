cbe36f2f585090679c3f76368199be811a837246
Jason Nathannael <Jasonnathannael05@gmail.com>
2024-12-06 17:03:34 +0700

    membuat autentikasi untuk login

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const User = require('./models/User');
-
+const bcrypt = require('bcryptjs');
 const Album = require("./models/album");
 
 
@@ -48,16 +48,37 @@ app.post("/api/register", async (req, res) => {
   const { username, email, password, confirmPassword } = req.body;
 
   if (password !== confirmPassword) {
-    return res.status(400).json({ message: "Passwords don't match" });
+    return res.status(400).json({ message: "Password tidak cocok" });
   }
 
   const userExists = await User.findOne({ email });
   if (userExists) {
-    return res.status(400).json({ message: "Email already in use" });
+    return res.status(400).json({ message: "Email sudah digunakan" });
   }
 
-  const newUser = new User({ username, email, password });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, email, password});
   await newUser.save();
   
-  res.status(201).json({ message: 'User registered successfully' });
+  res.status(201).json({ message: 'Register berhasil' });
+});
+
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Email tidak ditemukan" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Password salah" });
+    }
+
+    res.status(200).json({ message: "Login berhasil", user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
